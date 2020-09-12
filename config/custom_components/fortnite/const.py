1273737346e1b953533953dfae266850318e9c23
Michael Lunzer <michaellunzer@users.noreply.github.com>
2020-09-11 23:47:27 -0700

    refactored fortnite integration with major help from @clyra - working

--- config/custom_components/fortnite/const.py
+++
@@ -1,3 +0,0 @@
-"""Constants for the fortnite_api integration."""
-
-DOMAIN = "fortnite"
--- config/custom_components/fortnite/sensor.py
+++ config/custom_components/fortnite/sensor.py
@@ -1,110 +1,78 @@
-"""Platform for sensor integration."""
-from homeassistant.helpers.entity import Entity
-from fortnite_python import Fortnite
-from fortnite_python.domain import Platform
-from fortnite_python.domain import Mode
+"""Fortnite Sensor integration
+to use, add something like this to HA config:
+
+sensor:
+ - platform: fortnite
+   name: myfortnite_integration
+   api_key: <your_api_key>
+   player_id: <player_name>
+   game_platform: "GAMEPAD"
+   game_mode: "SQUAD"
+"""
+
+import voluptuous as vol
+import logging
+
+from .fortnite import FortniteData
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import (
-    ATTR_ATTRIBUTION,
-    CONF_API_KEY,
-    CONF_NAME,
-    CONF_PLAYER,
-    CONF_GAME_PLATFORM,
-    CONF_MODE,
-)
+from homeassistant.const import CONF_NAME, CONF_API_KEY
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 
-REQUIREMENTS = ["fortnite-python==0.3.5"]
-
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = "Fortnite API Sensor"
+DOMAIN = "sensor"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_API_KEY): cv.string,
-        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-        vol.Required(CONF_PLAYER): cv.string,
-        vol.Required(CONF_GAME_PLATFORM): cv.string,
-        vol.Required(CONF_MODE): cv.string,
+        vol.Required("player_id"): cv.string,
+        vol.Required("game_platform"): cv.string,
+        vol.Required("game_mode"): cv.string,
+        vol.Required(CONF_NAME): cv.string,
     }
 )
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the sensor platform."""
+    """Set up the fortnite sensor."""
 
-    api_key = config.get(CONF_API_KEY)
+    _LOGGER.info("init sensor")
     name = config.get(CONF_NAME)
-    player = config.get(CONF_PLAYER)
-    game_platform = config.get(CONF_GAME_PLATFORM)
-    mode = config.get(CONF_MODE)
+    api_key = config.get(CONF_API_KEY)
+    player_id = config.get("player_id")
+    game_platform = config.get("game_platform")
+    game_mode = config.get("game_mode")
 
-    # data = ShodanData(shodan.Shodan(api_key), query)
+    fn = FortniteData(name, api_key, player_id, game_platform, game_mode)
 
-    fortnite = Fortnite(api_key)
-    player = fortnite.player(player, Platform.game_platform)
-    stats = player.get_stats(Mode.mode)
-    data = stats.top1  # pylint: disable=maybe-no-member
-    # hass.states.set("hello_fortnite.Hello_Fortnite", data)
+    if not fn:
+        _LOGGER.error("Unable to create the fortnite sensor")
+        return
 
-    add_entities([FortniteSquadsWinsSensor(name, data)], True)
+    add_entities([FortniteSensor(hass, fn)], True)
 
 
-class FortniteSquadsWinsSensor(Entity):
-    """Representation of a Sensor."""
-
-    def __init__(self):
-        """Initialize the sensor."""
-        self.data = data
-        self._name = "Fortnite Squads Wins"
-        self._state = None
-        self._unit_of_measurement = "Wins"
+class FortniteSensor(Entity):
+    def __init__(self, hass, fn):
+        self._hass = hass
+        self.data = fn
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return self._name
+        return "{}".format(self.data.name)
 
     @property
     def state(self):
-        """Return the state of the sensor."""
-        return self._state
+        """Return the state of the device."""
+        return self.data.attr["kills"]
 
     @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement."""
-        return self._unit_of_measurement
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        return self.data.attr
 
     def update(self):
-        """Fetch new state data for the sensor.
-
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        self.data.update()
-        self._state = self.data
-
-
-## This needs a lot of work and is not functioning!
-##### based off of this:    https://github.com/home-assistant/core/blob/dev/homeassistant/components/shodan/sensor.py
-
-
-class FortniteData:
-    """Get the latest data and update the states."""
-
-    def __init__(self, api, query):
-        """Initialize the data object."""
-        self._api = api
-        self._query = query
-        self.details = None
-
-    def update(self):
-        """Get the latest data from shodan.io."""
-        # self.details = self._api.count(self._query)
-        fortnite = Fortnite(api_key)
-        player = fortnite.player(player, Platform.game_platform)
-        stats = player.get_stats(Mode.mode)
-        data = stats.top1  # pylint: disable=maybe-no-member
-        # hass.states.set("hello_fortnite.Hello_Fortnite", data)
+        self.data.update_stats()
